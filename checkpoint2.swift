123f06a961624e86d92922cc00c3398238af188c
Doricio <43920572+viniciusdoricio@users.noreply.github.com>
2023-05-30 13:20:12 -0400

    Checkpoint 02

---
+++ checkpoint2.swift
@@ -0,0 +1,7 @@
+let demonSlayers = ["Tanjiro, Inosuke, Zenitsu"] // Created Array
+
+print(demonSlayers.count) // Counting number of itens in given Array
+
+set(demonSlayers) = slayers // Tranforming given Array into a Set for unique values counting
+
+print(slayers.count) // Counting number of itens in given Set
